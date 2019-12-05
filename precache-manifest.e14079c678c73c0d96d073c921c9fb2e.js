25ac4d1bbd05621ba9d46a2203230e494b6228c4
mo3az.abdellatif <mo3az.abdellatif@gmail.com>
2019-12-05 12:01:18 +0200

    Updates

---
+++ precache-manifest.e14079c678c73c0d96d073c921c9fb2e.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "586677ec2054fa2dbf94c1220b4998f9",
+    "url": "/ReactTODO/index.html"
+  },
+  {
+    "revision": "77fac3b4014459d1457d",
+    "url": "/ReactTODO/static/css/2.2ecb51dc.chunk.css"
+  },
+  {
+    "revision": "a8975dace76f47ca81e9",
+    "url": "/ReactTODO/static/css/main.ff7f8436.chunk.css"
+  },
+  {
+    "revision": "77fac3b4014459d1457d",
+    "url": "/ReactTODO/static/js/2.4095d2cb.chunk.js"
+  },
+  {
+    "revision": "a8975dace76f47ca81e9",
+    "url": "/ReactTODO/static/js/main.28ebbef9.chunk.js"
+  },
+  {
+    "revision": "f633a0fc7e7d421147d4",
+    "url": "/ReactTODO/static/js/runtime-main.c2750f0c.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/ReactTODO/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/ReactTODO/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/ReactTODO/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/ReactTODO/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/ReactTODO/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
